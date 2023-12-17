50b68e0f2d60ced27fe3de15c525c6f5e2773ff6
kanenorman <kanenorman1996@gmail.com>
2023-12-17 17:59:49 -0600

    Foundation of new website

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,15 @@ document.addEventListener("DOMContentLoaded", function () {
     navigation: true,
     navigationPosition: "right",
     licenseKey: "gplv3-license",
-    autoScrolling: true,
+    anchors: ["home", "about", "projects"],
+    navigationTooltips: ["Home", "About", "Projects"],
   });
+
+  const messages = [
+    "Hello, welcome to my website!",
+    "Im a full stack engineer with a deep passion for data and Machine Learning!",
+    "Feel free to look around and contact me if you have any questions!",
+  ];
+  const typingEffect = new TypingEffect("typed-message", messages, 50, 30);
+  typingEffect.typeAndEraseMessages();
 });
--- assets/js/shell-typing.js
+++ assets/js/shell-typing.js
@@ -1,54 +1,68 @@
-const messageElement = document.getElementById("typed-message");
-const messages = [
-  "Hello, this is message 1.",
-  "This is message 2.",
-  "And here's message 3.",
-];
-const typingSpeed = 50; // Adjust typing speed (characters per second)
-const erasingSpeed = 30; // Adjust erasing speed (characters per second)
+class TypingEffect {
+  constructor(elementId, messages, typingSpeed, erasingSpeed, pauseBeforeFirstMessage=1000) {
+    this.element = document.getElementById(elementId);
+    this.messages = messages;
+    this.typingSpeed = typingSpeed;
+    this.erasingSpeed = erasingSpeed;
+    this.pauseBeforeFirstMessage = pauseBeforeFirstMessage;
+  }
 
-function typeAndEraseMessages(index) {
-  if (index < messages.length) {
-    const currentMessage = messages[index];
-    if (index === messages.length - 1) {
-      // For the last message, just type it without erasing
-      typeMessage(currentMessage, 0, () => {
+  typeAndEraseMessages(index = 0) {
+    if (index < this.messages.length) {
+      const currentMessage = this.messages[index];
+      const isLastMessage = index === this.messages.length - 1;
+
+      // Pause before typing the first message
+      if (index === 0) {
         setTimeout(() => {
-          typeAndEraseMessages(index + 1);
-        }, 1000); // Delay before typing the next message
-      });
-    } else {
-      typeMessage(currentMessage, 0, () => {
-        eraseMessage(currentMessage.length, () => {
-          typeAndEraseMessages(index + 1);
+          this.typeMessage(currentMessage, 0, () => {
+            if (isLastMessage) {
+              setTimeout(() => {
+                this.typeAndEraseMessages(index + 1);
+              }, 1000); // Delay before repeating
+            } else {
+              this.eraseMessage(currentMessage.length, () => {
+                this.typeAndEraseMessages(index + 1);
+              });
+            }
+          });
+        }, this.pauseBeforeFirstMessage);
+      } else {
+        // No pause for messages other than the first one
+        this.typeMessage(currentMessage, 0, () => {
+          if (isLastMessage) {
+            setTimeout(() => {
+              this.typeAndEraseMessages(index + 1);
+            }, 1000); // Delay before repeating
+          } else {
+            this.eraseMessage(currentMessage.length, () => {
+              this.typeAndEraseMessages(index + 1);
+            });
+          }
         });
-      });
+      }
+    }
+  }
+
+  typeMessage(message, index, callback) {
+    if (index < message.length) {
+      this.element.textContent += message.charAt(index);
+      setTimeout(() => {
+        this.typeMessage(message, index + 1, callback);
+      }, this.typingSpeed);
+    } else {
+      setTimeout(callback, 1000); // Delay before erasing or typing the next message
+    }
+  }
+
+  eraseMessage(length, callback) {
+    if (length > 0) {
+      this.element.textContent = this.element.textContent.slice(0, -1);
+      setTimeout(() => {
+        this.eraseMessage(length - 1, callback);
+      }, this.erasingSpeed);
+    } else {
+      setTimeout(callback, 1000); // Delay before typing the next message
     }
   }
 }
-
-function typeMessage(message, index, callback) {
-  if (index < message.length) {
-    messageElement.textContent += message.charAt(index);
-    setTimeout(function () {
-      typeMessage(message, index + 1, callback);
-    }, typingSpeed);
-  } else {
-    setTimeout(callback, 1000); // Delay before erasing or typing the next message
-  }
-}
-
-function eraseMessage(length, callback) {
-  if (length > 0) {
-    const currentMessage = messageElement.textContent;
-    messageElement.textContent = currentMessage.slice(0, -1);
-    setTimeout(function () {
-      eraseMessage(length - 1, callback);
-    }, erasingSpeed);
-  } else {
-    setTimeout(callback, 1000); // Delay before typing the next message
-  }
-}
-
-
-typeAndEraseMessages(0);
