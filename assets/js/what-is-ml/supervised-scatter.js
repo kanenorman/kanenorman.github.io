2969d723906624c77c16a84e219c368c322e678a
kanenorman <kanenorman1996@gmail.com>
2024-06-27 10:53:16 -0500

    Change dataset

--- assets/js/what-is-ml/supervised-scatter.js
+++ assets/js/what-is-ml/supervised-scatter.js
@@ -20,12 +20,12 @@ const PlotSetup = {
   },
 
   createAxes(svg, data, width, height) {
-    const heightExtent = d3.extent(data, (d) => d.height);
-    const weightExtent = d3.extent(data, (d) => d.weight);
+    const xExtent = d3.extent(data, (d) => d.x);
+    const yExtent = d3.extent(data, (d) => d.y);
 
     const x = d3
       .scaleLinear()
-      .domain([heightExtent[0] - 10, heightExtent[1] + 10])
+      .domain([xExtent[0] - 3 , xExtent[1] + 3])
       .range([0, width]);
 
     svg
@@ -35,7 +35,7 @@ const PlotSetup = {
 
     const y = d3
       .scaleLinear()
-      .domain([weightExtent[0] - 10, weightExtent[1] + 10])
+      .domain([yExtent[0] - 10, yExtent[1] + 10])
       .range([height, 0]);
 
     svg.append("g").call(d3.axisLeft(y));
@@ -77,8 +77,8 @@ const PlotSetup = {
       .transition()
       .delay((d, i) => Math.floor(i / 10) * 100) // Delay each batch of 10 dots based on index
       .duration(1000)
-      .attr("cx", (d) => x(d.height))
-      .attr("cy", (d) => y(d.weight));
+      .attr("cx", (d) => x(d.x))
+      .attr("cy", (d) => y(d.y));
   },
 
   scatterPoints(svg, data, x, y, width) {
@@ -88,8 +88,8 @@ const PlotSetup = {
       .data(data)
       .enter()
       .append("circle")
-      .attr("cx", (d) => x(d.height))
-      .attr("cy", (d) => y(d.weight))
+      .attr("cx", (d) => x(d.x))
+      .attr("cy", (d) => y(d.y))
       .attr("r", 5)
       .attr("class", "dot")
       .attr("fill", "#f4b334")
@@ -117,58 +117,15 @@ const PlotSetup = {
 };
 
 const HeightWeightScatter = {
-  data: [
-    { height: 168.73, weight: 88.06 },
-    { height: 197.54, weight: 99.62 },
-    { height: 186.6, weight: 92.72 },
-    { height: 179.93, weight: 88.46 },
-    { height: 157.8, weight: 71.51 },
-    { height: 157.8, weight: 75.3 },
-    { height: 152.9, weight: 74.15 },
-    { height: 193.31, weight: 101.94 },
-    { height: 180.06, weight: 91.75 },
-    { height: 185.4, weight: 83.89 },
-    { height: 151.03, weight: 77.14 },
-    { height: 198.5, weight: 97.32 },
-    { height: 191.62, weight: 92.43 },
-    { height: 160.62, weight: 83.37 },
-    { height: 159.09, weight: 84.7 },
-    { height: 159.17, weight: 84.24 },
-    { height: 165.21, weight: 78.41 },
-    { height: 176.24, weight: 86.57 },
-    { height: 171.6, weight: 87.45 },
-    { height: 164.56, weight: 87.16 },
-    { height: 180.59, weight: 87.9 },
-    { height: 156.97, weight: 77.56 },
-    { height: 164.61, weight: 76.77 },
-    { height: 168.32, weight: 78.18 },
-    { height: 172.8, weight: 90.46 },
-    { height: 189.26, weight: 101.41 },
-    { height: 159.98, weight: 79.63 },
-    { height: 175.71, weight: 92.87 },
-    { height: 179.62, weight: 91.62 },
-    { height: 152.32, weight: 72.94 },
-    { height: 180.38, weight: 92.0 },
-    { height: 158.53, weight: 86.95 },
-    { height: 153.25, weight: 76.45 },
-    { height: 197.44, weight: 106.55 },
-    { height: 198.28, weight: 86.04 },
-    { height: 190.42, weight: 99.32 },
-    { height: 165.23, weight: 83.05 },
-    { height: 154.88, weight: 75.95 },
-    { height: 184.21, weight: 92.56 },
-    { height: 172.01, weight: 76.07 },
-    { height: 156.1, weight: 76.95 },
-    { height: 174.76, weight: 89.16 },
-    { height: 151.72, weight: 83.25 },
-    { height: 195.47, weight: 95.14 },
-    { height: 162.94, weight: 77.43 },
-    { height: 183.13, weight: 89.05 },
-    { height: 165.59, weight: 87.37 },
-    { height: 176.0, weight: 89.65 },
-    { height: 177.34, weight: 86.02 },
-    { height: 159.24, weight: 82.19 },
-  ],
+  data: Array.from({ length: 50 }, () => {
+    const slope = 2; // True slope of the relationship
+    const intercept = 5; // True intercept of the relationship
+    const noise = 9; // Amount of noise to add
+    const x =  3 + Math.random() * 10;
+    const y = slope * x + intercept + (Math.random() - 0.5) * noise;
+    return { x, y };
+  }),
+
   initialized: false,
 
   plot: function () {
@@ -186,10 +143,20 @@ const LinearRegressionPlot = {
   m: 0.0, // Slope of the regression line
   b: 0.0, // Intercept of the regression line
   epoch: 0,
-  learningRate: 0.001,
-  max_epochs: 10,
+  learningRate: 0.0001,
+  maxEpochs: 100,
   data: HeightWeightScatter.data,
   n: HeightWeightScatter.data.length,
+
+  // MSE Loss
+  computeError() {
+    let totalError = 0;
+    this.data.forEach(d => {
+      const yPred = this.predict(d.x);
+      totalError += (d.y - yPred) ** 2;
+    });
+    return totalError / this.n
+  },
 
   predict: function (x) {
     return this.m * x + this.b;
@@ -199,9 +166,9 @@ const LinearRegressionPlot = {
     let mGradient = 0;
     let bGradient = 0;
     this.data.forEach((d) => {
-      const yPred = this.predict(d.height);
-      const error = d.weight - yPred;
-      mGradient += -(2 / this.n) * d.height * error;
+      const yPred = this.predict(d.x);
+      const error = d.y - yPred;
+      mGradient += -(2 / this.n) * d.x * error;
       bGradient += -(2 / this.n) * error;
     });
     console.log("mGradient: ", mGradient, "bGradient: ", bGradient);
@@ -260,34 +227,45 @@ const LinearRegressionPlot = {
 
   plot: function () {
     if (!this.initialized) {
+      this.initialized = true;
       const { svg, width, height } = PlotSetup.getCommonSetup(
         "linear-regression-plot",
       );
       const { x, y } = PlotSetup.createAxes(svg, this.data, width, height);
 
-      const runEpoch = () => {
-        if (this.epoch < this.max_epochs) {
-          this.gradientDescent();
-          this.epoch++;
-          console.log(
-            "Epoch: ",
-            this.epoch,
-            "Max: ",
-            this.max_epochs,
-            "m: ",
-            this.m,
-            "b: ",
-            this.b,
+      const runGradientDescent = () => {
+
+        for (let i = 0; i < this.maxEpochs; i++) {
+
+          setTimeout(() => {
+
+            this.gradientDescent()
+            console.log("m", this.m, "b", this.b, "mse", this.computeError())
+
+            this.updateRegressionPlot(svg, x, y, width, height)
+
+
+          }, i * 10
+
+
           );
-          this.updateRegressionPlot(svg, x, y, width, height);
-          requestAnimationFrame(runEpoch);
         }
+
       };
 
-      // Initialize the plot
-      this.updateRegressionPlot(svg, x, y, width, height);
-      this.initialized = true;
-      runEpoch();
+      runGradientDescent();
     }
   },
 };
+
+
+console.log(
+  "Epoch: ",
+  this.epoch,
+  "Max: ",
+  this.maxEpochs,
+  "m: ",
+  this.m,
+  "b: ",
+  this.b,
+);
